286b34b7cd29229841b218d3511d88871c293407
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-06-05 01:47:33 +0200

    Clean ColsAndRowsFactoryTest

--- src/test/java/spr/ex/ColsAndRowsFactoryTest.java
+++ src/test/java/spr/ex/ColsAndRowsFactoryTest.java
@@ -2,48 +2,45 @@ package spr.ex;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static spr.ex.ColsAndRowsFactory.parseColsAndRows;
 
 class ColsAndRowsFactoryTest {
     @Test
+    void shouldParse() {
+        // when
+        ColsAndRows actual = parseColsAndRows("123:456");
+
+        // then
+        assertEquals(123, actual.cols);
+        assertEquals(456, actual.rows);
+    }
+
+    @Test
     void shouldThrowIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows(":"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("4::4"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("asd"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("4:d"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("1:1a"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("2: 7"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("  31:1"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("a:a"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("10:"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows(":10"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows(""));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("22;22"));
-        assertThrows(IllegalArgumentException.class, () -> ColsAndRowsFactory.parseColsAndRows("."));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows(""));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("."));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows(":"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("10:"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows(":10"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("2: 7"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("4::4"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("  31:1"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("22;22"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("4:d"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("a:a"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("1:1a"));
+        assertThrows(IllegalArgumentException.class, () -> parseColsAndRows("asd"));
     }
 
     @Test
-    void shouldThrowZeroDimensionException() {
-        assertThrows(ZeroDimensionException.class, () -> ColsAndRowsFactory.parseColsAndRows("0:0"));
+    void shouldThrowZeroDimensionExceptionCols() {
+        assertThrows(ZeroDimensionException.class, () -> parseColsAndRows("0:1"));
     }
 
     @Test
-    void shouldParseColsAndRows_smallAmount() {
-        // when
-        ColsAndRows actual = ColsAndRowsFactory.parseColsAndRows("1:2");
-
-        // then
-        assertEquals(1, actual.cols);
-        assertEquals(2, actual.rows);
-    }
-
-    @Test
-    void shouldParseColsAndRows_bigAmount() {
-        // when
-        ColsAndRows actual = ColsAndRowsFactory.parseColsAndRows("9999999:8888888");
-
-        // then
-        assertEquals(9999999, actual.cols);
-        assertEquals(8888888, actual.rows);
+    void shouldThrowZeroDimensionExceptionRows() {
+        assertThrows(ZeroDimensionException.class, () -> parseColsAndRows("1:0"));
     }
 }
